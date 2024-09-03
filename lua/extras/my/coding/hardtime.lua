ad104c46d74a54dc246b1ad5ffe1f6a5d8778f41
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-09-03 15:25:12 +0200

    feat(coding): setup hardtime.nvim

---
+++ lua/extras/my/coding/hardtime.lua
@@ -0,0 +1,21 @@
+return {
+  'm4xshen/hardtime.nvim',
+  event = 'VeryLazy',
+  opts = function()
+    local hardtime = require('hardtime')
+
+    local toggle = LazyVim.toggle.wrap({
+      name = 'Hard time',
+      get = function()
+        return hardtime.is_plugin_enabled
+      end,
+      set = function()
+        hardtime.toggle()
+      end,
+    })
+
+    LazyVim.toggle.map('<leader>uH', toggle)
+
+    return {}
+  end,
+}
