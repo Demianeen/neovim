e29758d9d9e7bad307f9a6b4d3e242c7eeb32f6b
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-09-03 15:30:04 +0200

    feat(ui): add which key ui options from aimuzov

---
+++ lua/extras/my/ui/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'folke/which-key.nvim',
+    opts = {
+      layout = { spacing = 5 },
+      plugins = { spelling = { enabled = false } },
+      win = { wo = { winblend = 15 } },
+    },
+  },
+}
