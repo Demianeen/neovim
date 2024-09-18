c52f5fe8748c09c915b076d124749eaa6cc312ab
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-09-18 08:45:26 +0100

    feat(editor): move from nvim-colorizer to nvim-highlight-colors

---
+++ lua/extras/my/editor/highlight-colors.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'brenoprata10/nvim-highlight-colors',
+    event = 'VeryLazy',
+    opts = {
+      virtual_symbol = '󱓻 ',
+      render = 'virtual',
+      virtual_symbol_position = 'inline',
+      virtual_symbol_suffix = '',
+      enable_short_hex = false,
+      enable_named_colors = false,
+      enable_tailwind = true,
+    },
+  },
+}
