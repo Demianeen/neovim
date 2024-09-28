2514c495014f936a5e7e2ad636772aea4d05f8e2
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-09-28 11:50:46 +0100

    feat(editor): try different gitsigns diff opts

--- lua/extras/my/editor/gitsigns.lua
+++ lua/extras/my/editor/gitsigns.lua
@@ -1,11 +1,15 @@
 return {
-	{
-		'lewis6991/gitsigns.nvim',
-		opts = {
-			sign_priority = 13,
-			current_line_blame = true,
-			current_line_blame_opts = { delay = 250 },
-			current_line_blame_formatter = ' 󰞗 <author>  <author_time:%R>  <summary>',
-		},
-	},
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      sign_priority = 13,
+      current_line_blame = true,
+      current_line_blame_opts = { delay = 250 },
+      current_line_blame_formatter = ' 󰞗 <author>  <author_time:%R>  <summary>',
+      diff_opts = {
+        algorithm = 'histogram',
+        ignore_whitespace_change = true,
+      },
+    },
+  },
 }
