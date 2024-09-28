05482edc4492990436fa50d410ce3c8337b22729
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-09-28 11:41:48 +0100

    feat(editor): move to a different yazi plugin

--- lua/extras/my/editor/yazi.lua
+++ lua/extras/my/editor/yazi.lua
@@ -1,10 +1,21 @@
 return {
-  'DreamMaoMao/yazi.nvim',
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
-    'nvim-lua/plenary.nvim',
-  },
+  'mikavilpas/yazi.nvim',
+  event = 'VeryLazy',
   keys = {
-    { '<leader>fy', '<cmd>Yazi<CR>', desc = 'Toggle Yazi' },
+    { '<leader>fy', '<cmd>Yazi<CR>', desc = 'Open yazi at the current file' },
+    {
+      '<leader>fY',
+      '<cmd>Yazi toggle<CR>',
+      desc = 'Resume the last yazi session',
+    },
+  },
+  opts = {
+    open_for_directories = true,
+    keymaps = {
+      show_help = '<c-h>',
+    },
+    integrations = {
+      resolve_relative_path_application = 'realpath',
+    },
   },
 }
