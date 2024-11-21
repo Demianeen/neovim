b8cb5b3b1f939d511ae288787f6e3a5b1e6d9b29
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-11-21 11:07:44 +0000

    fix(editor/telescope): smart_open f command for snacks dashboard

--- lua/extras/my/editor/telescope/smart_open.lua
+++ lua/extras/my/editor/telescope/smart_open.lua
@@ -3,6 +3,7 @@ Smart_open_command = 'Telescope smart_open theme=dropdown previewer=false'
 return {
   {
     'danielfalk/smart-open.nvim',
+    lazy = false,
     branch = '0.2.x',
     dependencies = {
       'kkharji/sqlite.lua',
@@ -39,28 +40,28 @@ return {
       },
     },
   },
-  {
-    'nvimdev/dashboard-nvim',
-    optional = true,
-    opts = function(_, opts)
-      local projects = {
-        action = Smart_open_command,
-        desc = '  Find files',
-        icon = '',
-        key = 'f',
-      }
-
-      projects.desc = projects.desc .. string.rep(' ', 43 - #projects.desc)
-      projects.key_format = '  %s'
-
-      -- remove lazyvim config property
-      for i = #opts.config.center, 1, -1 do
-        if opts.config.center[i].key == 'f' then
-          table.remove(opts.config.center, i)
-        end
-      end
-
-      table.insert(opts.config.center, 1, projects)
-    end,
-  },
+  -- {
+  --   'nvimdev/dashboard-nvim',
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     local projects = {
+  --       action = Smart_open_command,
+  --       desc = '  Find files',
+  --       icon = '',
+  --       key = 'f',
+  --     }
+  --
+  --     projects.desc = projects.desc .. string.rep(' ', 43 - #projects.desc)
+  --     projects.key_format = '  %s'
+  --
+  --     -- remove lazyvim config property
+  --     for i = #opts.config.center, 1, -1 do
+  --       if opts.config.center[i].key == 'f' then
+  --         table.remove(opts.config.center, i)
+  --       end
+  --     end
+  --
+  --     table.insert(opts.config.center, 1, projects)
+  --   end,
+  -- },
 }
