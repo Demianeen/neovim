86524ee67cb62935da62b34e2b3cafff981f4150
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-11-27 10:45:28 +0000

    feat(editor): add i18n support

--- lua/extras/my/editor/i18n.lua
+++ lua/extras/my/editor/i18n.lua
@@ -1,5 +1,34 @@
 return {
-  'olrtg/nvim-i18n',
-  dependencies = { 'MunifTanjim/nui.nvim' },
-  config = true,
+  'nabekou29/js-i18n.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    { '<leader>ct', '<cmd>I18nEditTranslation<cr>', desc = 'Edit Translation' },
+  },
+  opts = function()
+    local i18n_config = require('js-i18n.config')
+
+    Snacks.toggle
+      .new({
+        name = 'I18n virtual text',
+        get = function()
+          return i18n_config.config.virt_text.enabled
+        end,
+        set = function()
+          vim.cmd('I18nVirtualTextToggle')
+        end,
+      })
+      :map('<leader>ut')
+
+    return {
+      primary_language = { 'en' },
+      virt_text = {
+        conceal_key = true,
+      },
+    }
+  end,
 }
