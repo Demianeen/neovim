ebb4aee43df5d8f8d105a0ef1c1a5751218a819e
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-03-30 10:25:39 +0100

    chore(lazyvim): enable copilot extra

--- lua/extras/my/coding/blink.lua
+++ lua/extras/my/coding/blink.lua
@@ -1,16 +1,25 @@
 return {
-  'saghen/blink.cmp',
-  opts = {
-    sources = {
-      providers = {
-        lsp = {
-          name = 'LSP',
-          module = 'blink.cmp.sources.lsp',
-          transform_items = function(_, items)
-            return vim.tbl_filter(function(item)
-              return item.kind ~= 14
-            end, items)
-          end,
+  {
+    'saghen/blink.cmp',
+    opts = {
+      keymap = {},
+    },
+  },
+  -- remove constants from autocomplete (if, else, while, etc.)
+  {
+    'saghen/blink.cmp',
+    opts = {
+      sources = {
+        providers = {
+          lsp = {
+            name = 'LSP',
+            module = 'blink.cmp.sources.lsp',
+            transform_items = function(_, items)
+              return vim.tbl_filter(function(item)
+                return item.kind ~= 14
+              end, items)
+            end,
+          },
         },
       },
     },
