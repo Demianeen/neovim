afb4aa4bc639cdcd119ce821e6a24855bcef2b8d
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-03-30 14:56:12 +0100

    feat: bump snippet score a bit for blink.cmp

--- lua/extras/my/coding/blink.lua
+++ lua/extras/my/coding/blink.lua
@@ -2,21 +2,21 @@ return {
   {
     'saghen/blink.cmp',
     opts = {
-      keymap = {},
-    },
-  },
-  -- remove constants from autocomplete (if, else, while, etc.)
-  {
-    'saghen/blink.cmp',
-    opts = {
       sources = {
         providers = {
+          -- bump snippet score a bit
+          snippets = {
+            name = 'Snippets',
+            module = 'blink.cmp.sources.snippets',
+            score_offset = -2, -- default is -3
+          },
+          -- remove constants from autocomplete (if, else, while, etc.)
           lsp = {
             name = 'LSP',
             module = 'blink.cmp.sources.lsp',
             transform_items = function(_, items)
               return vim.tbl_filter(function(item)
-                return item.kind ~= 14
+                return item.kind ~= vim.lsp.protocol.CompletionItemKind.Keyword
               end, items)
             end,
           },
