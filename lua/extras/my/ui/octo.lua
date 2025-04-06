dca1a86fa08ea8b97b52a7d2e9cbc4c3a6ece778
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2025-04-06 11:15:46 +0100

    feat(octo): setup

--- lua/extras/my/ui/octo.lua
+++ lua/extras/my/ui/octo.lua
@@ -1,14 +1,22 @@
 return {
-  'pwntester/octo.nvim',
-  cmd = 'Octo',
-  opts = {
-    gh_env = function()
-      local github_token = require('op').get_secret('GitHub', 'token')
-      if not github_token or not vim.startswith(github_token, 'ghp_') then
-        error('Failed to get GitHub token.')
-      end
+  {
+    'pwntester/octo.nvim',
+    cmd = 'Octo',
+    opts = {
+      gh_env = function()
+        local github_token = require('op').get_secret('GitHub', 'token')
+        if not github_token or not vim.startswith(github_token, 'ghp_') then
+          error('Failed to get GitHub token.')
+        end
 
-      return { GITHUB_TOKEN = github_token }
-    end,
+        return { GITHUB_TOKEN = github_token }
+      end,
+    },
+    {
+      'm4xshen/hardtime.nvim',
+      opts = function(_, opts)
+        table.insert(opts.disabled_filetypes, { 'octo' })
+      end,
+    },
   },
 }
